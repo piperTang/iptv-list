5f9a4416f5af43be39f578fcd06a05316705fcd9
root <root@1Panel.pve>
2023-10-20 08:03:47 +0000

    1panel自动提交-20231020

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 获取当前日期，格式为“年月日”
+current_date=$(date +'%Y%m%d')
+
+# 提交变更并添加带有时间戳的 commit 注释
+git add .
+git commit -m "1panel自动提交-${current_date}"
+
+# 推送到远程仓库（如果需要）
+git push origin main
+
+# 输出提交成功消息
+echo "变更已提交到 Git 仓库。"
